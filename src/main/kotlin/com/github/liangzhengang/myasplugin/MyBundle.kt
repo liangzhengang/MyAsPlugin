7595dadfea9ad5c535b879e6c5a9ff6dbb6e4598
GitHub Action <action@github.com>
2021-12-07 06:38:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/liangzhengang/myasplugin/MyBundle.kt
+++ src/main/kotlin/com/github/liangzhengang/myasplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.liangzhengang.myasplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/liangzhengang/myasplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/liangzhengang/myasplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.liangzhengang.myasplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liangzhengang.myasplugin.MyBundle
 
 class MyApplicationService {
 
